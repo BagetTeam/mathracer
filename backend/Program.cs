4e9009e053bcb7a9e38630c2587e1ad5c0d86f6c
mingli202 <vincentmingli@gmail.com>
2025-03-30 08:26:50 -0400

    add backend placeholder

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,10 @@
+using hub;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+app.MapHub<RacerHub>("/hub");
---
+++ backend/RacerHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace hub;
+
+public class RacerHub : Hub
+{
+    public async Task NewMessege(long username, string message) =>
+        await Clients.All.SendAsync("messageReceived", username, message);
+}
